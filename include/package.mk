1385eded2ffe8953fdc22f6bb1bbe3f875a4a37e
Philip Prindeville <philipp@redfish-solutions.com>
2017-04-16 20:44:29 -0600

    Back out removal of overlays:
        b044bd5 build: remove package makefile overlay functionality

--- include/package.mk
+++ include/package.mk
@@ -237,7 +237,14 @@ define Build/DefaultTargets
   endef
 endef
 
+define Build/IncludeOverlay
+  $(eval -include $(wildcard $(TOPDIR)/overlay/*/$(PKG_DIR_NAME).mk))
+  define Build/IncludeOverlay
+  endef
+endef
+
 define BuildPackage
+  $(Build/IncludeOverlay)
   $(eval $(Package/Default))
   $(eval $(Package/$(1)))
 
